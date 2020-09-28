83108df77c2c3289c4d79da59eea3609f551e966
Tomas De Amos <tomasdeamos@gmail.com>
2020-09-28 19:03:13 -0300

    Working v1

--- scrapper.py
+++ scrapper.py
@@ -11,20 +11,26 @@ import matplotlib.ticker as ticker
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
+import html5lib
 
-no_pages = 2
+no_pages = 1
 
 def get_data(pageNo):  
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
-    r = requests.get('https://www.amazon.in/gp/bestsellers/books/ref=zg_bs_pg_'+str(pageNo)+'?ie=UTF8&pg='+str(pageNo), headers=headers)#, proxies=proxies)
+    #r = requests.get('https://www.amazon.com/gp/bestsellers/books/ref=zg_bs_pg_'+str(pageNo)+'?ie=UTF8&pg='+str(pageNo), headers=headers)#, proxies=proxies)
+    r = requests.get('https://www.amazon.com/gp/bestsellers/books/ref=zg_bs_pg_', headers=headers)#, proxies=proxies)
     content = r.content
-    soup = BeautifulSoup(content)
-    #print(soup)
+    soup = BeautifulSoup(content, "html5lib")
 
+    #soup = BeautifulSoup(content, features="html5lib")
+    #print(soup)
+    #print("entra1")
+    #print(soup.findAll('div', attrs={'class':'a-section a-spacing-none aok-relative'}))
     alls = []
+    
     for d in soup.findAll('div', attrs={'class':'a-section a-spacing-none aok-relative'}):
-        #print(d)
+        
         name = d.find('span', attrs={'class':'zg-text-center-align'})
         n = name.find_all('img', alt=True)
         #print(n[0]['alt'])
@@ -36,7 +42,7 @@ def get_data(pageNo):
         all1=[]
 
         if name is not None:
-            #print(n[0]['alt'])
+            print(n[0]['alt'])
             all1.append(n[0]['alt'])
         else:
             all1.append("unknown-product")
@@ -68,10 +74,11 @@ def get_data(pageNo):
             all1.append(price.text)
         else:
             all1.append('0')
-        alls.append(all1)    
+        alls.append(all1)            
+    
     return alls
 
-    results = []
+results = []
     
 for i in range(1, no_pages+1):
     results.append(get_data(i))
--- test.py
+++ test.py
@@ -1,2 +1,26 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline
+import re
+import time
+from datetime import datetime
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
 
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+r = requests.get('https://www.amazon.com/gp/bestsellers/books/ref=zg_bs_pg_', headers=headers)#, proxies=proxies)
+content = r.content
+soup = BeautifulSoup(content)
+
+print("entra0")
+
+#for d in soup.findAll('div', attrs={'class':'a'}):
+for d in soup.findAll('div', attrs={'class':'a-section a-spacing-none aok-relative'}, recursive=True):
+    print(d)
+#for d in soup.findAll('div', recursive=True):
+    
